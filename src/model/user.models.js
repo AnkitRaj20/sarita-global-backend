bc11dfb75521c15a72b91dc51669f3484fc8ffb2
Ankit Raj <ankitraja177@gmail.com>
2024-05-23 17:20:03 +0530

    added user model

--- src/model/user.models.js
+++ src/model/user.models.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 const userSchema = new Schema(
   {
@@ -26,5 +29,34 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+
+// Runs before saving the user to the database
+// pre is a hook
+userSchema.pre("save", async function (next) {
+  // If password is modified then run this code
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+// Check if the password is correct or not
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateAccessToken = async function(){
+  return jwt.sign({
+      _id:this._id,
+      email:this.email,
+      name:this.name
+  },
+  process.env.ACCESS_TOKEN_SECRET,
+  {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+  }
+  )
+}
 
 export const User = mongoose.model("User", userSchema);
